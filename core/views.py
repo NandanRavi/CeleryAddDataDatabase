b64837d26d53a44f0b356ddcee21a45f784b3ce8
ravi nandan <monubibhu@gmail.com>
2024-07-03 17:13:02 +0530

    adding limit to tasks

--- core/views.py
+++ core/views.py
@@ -58,10 +58,8 @@ class BooksView(APIView):
 class TriggerRatingUpdateView(View):
     def get(self, request, *args, **kwargs):
         start_id = kwargs.get('start_id', 100)
-        a=update_books_ratings.delay(start_id)
-        print(200)
-        b=update_books_ratings.delay(200)
-        # print(a)
+        update_books_ratings.delay(start_id)
+        update_books_ratings.delay(200)
         # print(start_id)
         return HttpResponse(f"Start id {start_id}")
 
